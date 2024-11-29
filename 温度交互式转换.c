2a562a61496da64fd15aebb4cf695c605029244c
JingXingYu <108406536+JingXingYu0@users.noreply.github.com>
2024-11-29 19:37:56 +0300

    使用函数规整了原程序
    还需要实现这两项，这次更新删除了这两项
    //\n目前要转换温度为开尔文,之后需要实现的内容
    //(输入其他字符或0查看公式并结束代码)\n，循环使用之后后需要实现的内容

--- 温度交互式转换.c
+++ 温度交互式转换.c
@@ -1,42 +1,33 @@
-// V=0.10
+// V=0.20
 #include <stdio.h>
+float munging(float temperature1, float temperature2,int choice);
 int main()
 {
-    int choice = 0;
-    float fahr, celsiu, kelvins;
-    celsiu = 0;
+    float temperature1;
+    float temperature2;
+    int choice;
     printf("(开尔文=1;华氏度=2;)\n请选择要从摄氏度转换的温度:");
     scanf("%d", &choice);
-    switch (choice)
-    {
-    case 1:
-        while (choice == 1, printf("(输入其他字符或0查看公式并结束代码)\n目前要转换温度为开尔文\n请输入要转换的摄氏度:"), (scanf("%f", &celsiu)) != EOF)
-        {
-
-            kelvins = celsiu + 273.15;
-            printf("转换的开尔文数为：%.2f°F\n", kelvins);
-            if (celsiu == 0)
-            {
-                printf("公式为:开尔文(°K)=摄氏度(°C)*5/9+32\n", kelvins);
-                break;
-            };
-        }
-        break;
-    case 2:
-    while (printf("(输入其他字符或0查看公式并结束代码)\n目前要转换的温度为华氏度\n请输入要转换的摄氏度:"), choice == 2, (scanf("%f", &celsiu)) != EOF)
-    {
-
-        fahr = celsiu / (5.0 / 9.0) + 32.0;
-        printf("转换的华氏度数为：%.2f°F\n", fahr);
-        if (celsiu == 0)
-        {
-            printf("公式为:华氏度(°F)=摄氏度(°C)*5/9+32\n", fahr);
-            break;
-        };
-    }
-    break;
-    default:
-        break;
-    }
+    printf("请输入要转换的摄氏度:");
+    scanf("%f", &temperature1);
+    printf("转换的开尔文数为：%.2f°F\n",  munging( temperature1,  temperature2, choice));
     return 0;
 }
+float munging(float temperature1, float temperature2,int choice)
+{
+        
+switch (choice)
+    {
+    case 1:
+    printf("公式为:开尔文(°K)=摄氏度(°C)*5/9+32\n");
+     temperature2 = temperature1 + 273.15;//摄氏度转开尔文
+    break;
+    case 2:
+    printf("公式为:华氏度(°F)=摄氏度(°C)*5/9+32\n");
+    temperature2 = temperature1 / (5.0 / 9.0) + 32.0;//摄氏度转华氏度
+    break;
+}
+return temperature2;
+}
+//\n目前要转换温度为开尔文,之后需要实现的内容
+//(输入其他字符或0查看公式并结束代码)\n，循环使用之后后需要实现的内容
