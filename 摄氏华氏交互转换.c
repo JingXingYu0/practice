8d042906dab670c81ff0e0486e265188008a3437
JingXingYu <108406536+JingXingYu0@users.noreply.github.com>
2024-11-29 19:34:51 +0300

    Add files via upload

--- 摄氏华氏交互转换.c
+++ 摄氏华氏交互转换.c
@@ -1,10 +1,19 @@
 #include <stdio.h>
 
-int main()
-{
-   float fahr,celsiu;
-   printf("请输入要转换的摄氏度:");
-   scanf("%f",&celsiu);
-   fahr =celsiu/(5.0/9.0)+32.0;
-   printf("转换的华氏度数为：%.2f\n",fahr);
+
+int main(){
+    float fahr,celsiu;
+    celsiu=0;
+    while ( printf("(输入其他字符或0查看公式并结束代码)\n请输入要转换的摄氏度:"),(scanf("%f",&celsiu)) != EOF) {
+
+        fahr =celsiu/(5.0/9.0)+32.0;   
+        printf("转换的华氏度数为：%.2f\n",fahr);
+        if (celsiu==0)
+        {   
+            printf("华氏度=摄氏度*5/9+32\n",fahr);
+            break;
+        };
+        
+    }
+return 0;
 }
