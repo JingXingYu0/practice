cb284ab94c106635be8a92192749d7e29e923666
JingXingYu <108406536+JingXingYu0@users.noreply.github.com>
2024-11-29 22:29:41 +0300

    循环使用更新
    优化代码逻辑，并精简代码
    还须制作的内容：能支持更多的单位换算;可使用其他单位换算;

--- 温度交互式转换.c
+++ 温度交互式转换.c
@@ -1,33 +1,31 @@
-// V=0.20
+// V=0.30
 #include <stdio.h>
-float munging(float temperature1, float temperature2,int choice);
+float TemperatureEnter(float temperature1);
 int main()
 {
-    float temperature1;
-    float temperature2;
-    int choice;
-    printf("(开尔文=1;华氏度=2;)\n请选择要从摄氏度转换的温度:");
-    scanf("%d", &choice);
-    printf("请输入要转换的摄氏度:");
-    scanf("%f", &temperature1);
-    printf("转换的开尔文数为：%.2f°F\n",  munging( temperature1,  temperature2, choice));
+    float temperature2 = 0;
+    int choice = 0;
+    while (printf("(结束代码=0;开尔文=1;华氏度=2;)\n请选择要从摄氏度转换的温度单位:"), scanf("%d", &choice))
+    {
+        switch (choice)
+        {
+        case 0:
+            return 0;
+        case 1:
+            temperature2 = TemperatureEnter(0) + 273.15; // 摄氏度转开尔文
+            printf("公式为:开尔文(°K)=摄氏度(°C)*5/9+32\n转换的开尔文数为:%.2f°F\n", temperature2);
+            break;
+        case 2:
+            temperature2 = TemperatureEnter(0) / (5.0 / 9.0) + 32.0; // 摄氏度转华氏度
+            printf("公式为:华氏度(°F)=摄氏度(°C)*5/9+32\n转换的华氏度数为:%.2f°F\n", temperature2);
+            break;
+        }
+    }
     return 0;
 }
-float munging(float temperature1, float temperature2,int choice)
+float TemperatureEnter(float temperature1)
 {
-        
-switch (choice)
-    {
-    case 1:
-    printf("公式为:开尔文(°K)=摄氏度(°C)*5/9+32\n");
-     temperature2 = temperature1 + 273.15;//摄氏度转开尔文
-    break;
-    case 2:
-    printf("公式为:华氏度(°F)=摄氏度(°C)*5/9+32\n");
-    temperature2 = temperature1 / (5.0 / 9.0) + 32.0;//摄氏度转华氏度
-    break;
+    printf("请输入要转换的摄氏度:");
+    scanf("%f", &temperature1);
+    return temperature1;
 }
-return temperature2;
-}
-//\n目前要转换温度为开尔文,之后需要实现的内容
-//(输入其他字符或0查看公式并结束代码)\n，循环使用之后后需要实现的内容
