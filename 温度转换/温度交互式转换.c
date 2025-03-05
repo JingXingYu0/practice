ce73e6312c76f1e31f36cee55932446f9b27fbab
JingXingYu <108406536+JingXingYu0@users.noreply.github.com>
2025-03-05 22:59:31 +0300

    更改了函数名称和变量名称，并优化了逻辑

--- 温度转换/温度交互式转换.c
+++ 温度转换/温度交互式转换.c
@@ -1,43 +1,48 @@
-// V=1.1
+// V=1.3
 #include <stdio.h>
 #include <stdlib.h>
 
-// compute函数用于处理温度转换
-float compute(char content[], float temperature_0, float temperature_1, int bool)
+// F_Temperature_Conversion_Output函数用于处理温度的输出
+float F_Temperature_Conversion_Output(char content[], float f_Temperature_Input, float f_Temperature_Output, int i_Step_Count)
 {
    float i;
-   if (bool == 0)
+   if (i_Step_Count == 0)
    {
-      i = temperature_0;
-      return temperature_0;
+      i = f_Temperature_Input;
+      return f_Temperature_Input;
    }
    else
    {
-      printf("转换的%s数为:%.2f°K\n", content, temperature_1);
-      return temperature_1;
+      printf("转换的%s数为:%.2f°K\n", content, f_Temperature_Output);
+      return f_Temperature_Output;
    }
 }
 
-// munging函数是主要的核心运算代码，根据输入类型进行温度转换
-float munging(char type, float temperature, int bool)
+// F_Temperature_Conversion_Judgment函数是主要的核心运算代码，根据输入类型进行温度转换
+float F_Temperature_Conversion_Judgment(char c_Input_Type, char c_Output_Type, float temperature, int i_Step_Count)
 {
+   char c_Conversion_Type = c_Input_Type;
+   if (i_Step_Count == 0)
+   {
+      F_Temperature_Conversion_Judgment(c_Output_Type, "\0", temperature, 1);
+   }
    float i;
-   switch (type)
+   switch (c_Conversion_Type)
    {
    case '1': // 开尔文转开尔文
    case 'K': // 开尔文转开尔文
    case 'k':
-      i = compute("开尔文", temperature, temperature, bool);
+      i = F_Temperature_Conversion_Output("开尔文", temperature, temperature, i_Step_Count);
       return i;
    case '2': // 开尔文转摄氏度
    case 'C': // 开尔文转摄氏度
    case 'c':
-      i = compute("摄氏度", temperature + 273.15, temperature - 273.15, bool);
+      i = F_Temperature_Conversion_Output("摄氏度", temperature + 273.15, temperature - 273.15, i_Step_Count);
       return i;
    case '3': // 开尔文转华氏度
    case 'F': // 开尔文转华氏度
    case 'f':
-      i = compute("华氏度", (temperature + 459.67) * 5 / 9, temperature * 9 / 5 - 459.67, bool);
+      i = F_Temperature_Conversion_Output("华氏度", (temperature + 459.67) * 5 / 9, temperature * 9 / 5 - 459.67, i_Step_Count);
       return i;
    default:
       printf("数据错误\n");
@@ -45,8 +50,8 @@ float munging(char type, float temperature, int bool)
    }
 }
 
-// User_choose函数作为用户输入字符串的简单集成
-char User_choose(char content[])
+// C_User_Choose函数作为用户输入字符串的简单集成
+char C_User_Choose(char content[])
 {
    char parameter;
    printf("%s\n", content);
@@ -55,36 +60,53 @@ char User_choose(char content[])
    return parameter;
 }
 
+// I_Interactive_Interface交互式界面使用了死循环
+int I_Interactive_Interface(float temperature)
+{
+   char f_Unit_Conversion_Input = '0';
+   char f_Unit_Conversion_Output = '0';
+   f_Unit_Conversion_Input = C_User_Choose("(0.结束代码;1.开尔文;2.摄氏度;3.华氏度;)\n请输入要从被转换的温度单位:");
+   if (f_Unit_Conversion_Input == '0')
+   {
+      return 1;
+   }
+   f_Unit_Conversion_Output = C_User_Choose("（开尔文=1;摄氏度=2;华氏度=3;)\n请输入要从需要转换的温度单位:");
+   printf("请输入要转换的温度数值:");
+   scanf("%f", &temperature);
+   getchar() != '\n';
+   F_Temperature_Conversion_Judgment(f_Unit_Conversion_Input, f_Unit_Conversion_Output, temperature, 0);
+}
 int main(int argc, char **argv)
 {
-   float temperature = 0;
-   char choice_1 = '0';
-   char choice_2 = '0';
+   float f_Temperature_Input = 0;
 
    // 如果参数符合特定条件，执行参数运行代码，否则进入交互式界面
-   if (argc >= 4 && argv[1][0] == '-' && argv[3][0] == '-')
+   if (argc !=1 &&argv[1][0] == '-' && (argv[1][1] == 'U' || argv[1][1] == 'u')) // 如果提供的是UI那么直接进入交互界面
    {
-      temperature = strtof(argv[2], NULL);
-      munging(argv[3][1], munging(argv[1][1], temperature, 0), 1);
-   }
-   else
-   {
-      // 如果没有参数，进入交互式界面
-      while (argc == 1)
+      while (1)
       {
-         char choice_1 = '0';
-         char choice_2 = '0';
-         choice_1 = User_choose("(0.结束代码;1.开尔文;2.摄氏度;3.华氏度;)\n请输入要从被转换的温度单位:");
-         if (choice_1 == '0')
+         if (I_Interactive_Interface(f_Temperature_Input)==1)
          {
             return 0;
          }
-         choice_2 = User_choose("（开尔文=1;摄氏度=2;华氏度=3;)\n请输入要从需要转换的温度单位:");
-         printf("请输入要转换的温度数值:");
-         scanf("%f", &temperature);
-         getchar() != '\n';
-         munging(choice_2, munging(choice_1, temperature, 0), 1);
+         
       }
    }
+   else
+   {
+      if (argc >= 4 && argv[1][0] == '-' && argv[3][0] == '-')//如果参数足够且可能可以使用则运行参数形式的代码，否则直接转向单次交互界面
+      {
+         char f_Unit_Conversion_Input = argv[1][1];
+         char f_Unit_Conversion_Output = argv[3][1];
+         f_Temperature_Input = strtof(argv[2], NULL);
+         F_Temperature_Conversion_Judgment(f_Unit_Conversion_Input, f_Unit_Conversion_Output, f_Temperature_Input, 0);
+      }
+      else
+      {
+         printf("温度转换Ｃ语言小程序。\n by:[JingXingYu]\n使用方法:加参数使用\n -U 使用交互式程序 \n -需要转换任意单位(K,C,F) 温度数值 - 被转换任意单位(K,C,F)");
+         I_Interactive_Interface(f_Temperature_Input);
+      }
+   }
+
    return 0;
 }
